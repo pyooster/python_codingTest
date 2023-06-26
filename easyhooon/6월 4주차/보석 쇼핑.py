8976749b093d0954cef78a6522984d27da34a534
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-06-26 20:33:53 +0900

    add: 보석 쇼핑

--- easyhooon/6월 4주차/보석 쇼핑.py
+++ easyhooon/6월 4주차/보석 쇼핑.py
@@ -2,26 +2,31 @@
 # 진열된 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간을 찾아서 구매
 # 3 부터 7 -> 모든 종류 보석 적어도 1개씩 포함
 # 슬라이딩 윈도우인가
-# 그런데 윈도우의 크기가 정해지지 않았음 그럼 아닐수도
+# 그런데 윈도우의 크기가 정해지지 않았음 그러면 슬라이딩 윈도우를 사용해서 문제를 풀 수 없음
 # 그러면 투포인터네
 import sys
 
 jew_type = {}
 
 
-def is_possible():
-    flag = True
-    for key in jew_type.keys():
-        if jew_type[key] < 1:
-            flag = False
-            return flag
-
-    return flag
+# 보석의 종류가 최대 10만개여서 10만 x 10만이 될 수 있음
+def is_possible(m, unique_cnt):
+    # flag = True
+    # for key in jew_type.keys():
+    #     if jew_type[key] < 1:
+    #         flag = False
+    #         return flag
+    #
+    return m == unique_cnt
 
 
 def solution(gems):
     # 진열대(배열)의 길이 List<String>
     n = len(gems)
+    # 보석의 종류의 개수
+    m = len(set(gems))
+    # 현재 유니크한 보석의 개수
+    unique_cnt = 0
     answer = []
     min_len_answer = sys.maxsize
     min_L = 0
@@ -34,25 +39,27 @@ def solution(gems):
     for L in range(0, n):
         if L >= 1:
             jew_type[gems[L - 1]] -= 1
+            if jew_type[gems[L - 1]] == 0:
+                unique_cnt -= 1
 
-        while R + 1 < n - 1 or not is_possible():
+        while R + 1 < n and not is_possible(m, unique_cnt):
             R += 1
-            print(R)
             jew_type[gems[R]] += 1
+            if jew_type[gems[R]] == 1:
+                unique_cnt += 1
 
-        print(L, R)
-        print(jew_type)
+        if is_possible(m, unique_cnt):
+            # print(L, R)
+            # print(jew_type)
 
-        len_answer = R - L + 1
-        if len_answer < min_len_answer:
-            min_len_answer = len_answer
-            min_L = L
-            min_R = R
+            len_answer = R - L + 1
+            if len_answer < min_len_answer:
+                min_len_answer = len_answer
+                min_L = L + 1  # 인덱스
+                min_R = R + 1
 
     answer = []
     answer.append(min_L)
     answer.append(min_R)
 
     return answer
-
-print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
